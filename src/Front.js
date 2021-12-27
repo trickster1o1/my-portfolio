afcd6ed3025b007f3da20ac6f79fe12f739b6cea
Nischal Tuladhar <nischaltuladhar15@gmail.com>
2021-12-27 19:00:30 +0545

    clean function call

--- src/Front.js
+++ src/Front.js
@@ -1,7 +1,10 @@
 import './main.css';
+import { useEffect } from 'react';
 function Front() {
-
-    let magic = () => {
+    useEffect(()=>{
+        document.getElementById('hm').classList.add('active');
+    },[]);
+    let magic = (actLink) => {
         const t1 = document.getElementById('t1');
         const t2 = document.getElementById('t2');
         const t3 = document.getElementById('t3');
@@ -9,61 +12,67 @@ function Front() {
         const t5 = document.getElementById('t5');
         const t6 = document.getElementById('t6');
         const fc = document.getElementById('fc');
-        t1.classList.remove('tb-1');
-        t1.classList.add('ta-1');
-        t2.classList.remove('tb-2');
-        t2.classList.add('ta-2');
-        t3.classList.remove('tb-3');
-        t3.classList.add('ta-3');
-        t4.classList.remove('tb-4');
-        t4.classList.add('ta-4');
-        t5.classList.remove('tb-5');
-        t5.classList.add('ta-5');
-        t6.classList.remove('tb-6');
-        t6.classList.add('ta-6');
-        fc.classList.remove('fccB');
-        fc.classList.add('fcc');
 
-        document.getElementById('abt').style.display = 'block';
-        document.getElementById('abt').classList.remove('fout');
-        document.getElementById('abt').classList.add('fin');
-    }
-    let backUp = () => {
-        const t1 = document.getElementById('t1');
-        const t2 = document.getElementById('t2');
-        const t3 = document.getElementById('t3');
-        const t4 = document.getElementById('t4');
-        const t5 = document.getElementById('t5');
-        const fc = document.getElementById('fc');
-        const t6 = document.getElementById('t6');
-        t1.classList.remove('ta-1');
-        t1.classList.add('tb-1');
-        t2.classList.remove('ta-2');
-        t2.classList.add('tb-2');
-        t3.classList.remove('ta-3');
-        t3.classList.add('tb-3');
-        t4.classList.remove('ta-4');
-        t4.classList.add('tb-4');
-        t5.classList.remove('ta-5');
-        t5.classList.add('tb-5');
-        t6.classList.remove('ta-6');
-        t6.classList.add('tb-6');
-        fc.classList.remove('fcc');
-        fc.classList.add('fccB');
+        if(actLink === 'about') {
+            document.getElementById('hm').classList.remove('active');
+            document.getElementById('cont').classList.remove('active');
+            document.getElementById('ab').classList.add('active');
+            t1.classList.remove('tb-1');
+            t1.classList.add('ta-1');
+            t2.classList.remove('tb-2');
+            t2.classList.add('ta-2');
+            t3.classList.remove('tb-3');
+            t3.classList.add('ta-3');
+            t4.classList.remove('tb-4');
+            t4.classList.add('ta-4');
+            t5.classList.remove('tb-5');
+            t5.classList.add('ta-5');
+            t6.classList.remove('tb-6');
+            t6.classList.add('ta-6');
+            fc.classList.remove('fccB');
+            fc.classList.add('fcc');
+
+            document.getElementById('abt').style.display = 'block';
+            document.getElementById('abt').classList.remove('fout');
+            document.getElementById('abt').classList.add('fin');
+
+        } else if(actLink === 'home') {
+            document.getElementById('ab').classList.remove('active');
+            document.getElementById('cont').classList.remove('active');
+            document.getElementById('hm').classList.add('active');
+            
+            t1.classList.remove('ta-1');
+            t1.classList.add('tb-1');
+            t2.classList.remove('ta-2');
+            t2.classList.add('tb-2');
+            t3.classList.remove('ta-3');
+            t3.classList.add('tb-3');
+            t4.classList.remove('ta-4');
+            t4.classList.add('tb-4');
+            t5.classList.remove('ta-5');
+            t5.classList.add('tb-5');
+            t6.classList.remove('ta-6');
+            t6.classList.add('tb-6');
+            fc.classList.remove('fcc');
+            fc.classList.add('fccB');
+            document.getElementById('abt').classList.remove('fin');
+            document.getElementById('abt').classList.add('fout');
+        } else {
+            document.getElementById('ab').classList.remove('active');
+            document.getElementById('hm').classList.remove('active');
+            document.getElementById('cont').classList.add('active');
+        }
         
-
-        document.getElementById('abt').classList.remove('fin');
-        document.getElementById('abt').classList.add('fout');
-
     }
+
     return(
         <>  
             <div className="navbar">
             <div className='nav-cont-1'>
                 <ul>
-                    <li className='nav' onClick={backUp}>Home</li>
-                    <li className='nav' onClick={magic}>About</li>
-                    <li className='nav'>Contacts</li>
+                    <li className='nav' onClick={()=>magic('home')} id="hm">Home</li>
+                    <li className='nav' onClick={()=>magic('about')} id="ab">About</li>
+                    <li className='nav' onClick={()=>magic('contact')} id='cont'>Contacts</li>
                 </ul>
             </div>
             </div>
