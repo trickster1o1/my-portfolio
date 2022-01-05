ba02958ee8bda6b8beab92c3b8caa83cc7a474eb
Nischal Tuladhar <nischaltuladhar15@gmail.com>
2022-01-05 19:45:23 +0545

    Game News Api Insertion

--- src/ApiTest.js
+++ src/ApiTest.js
@@ -1,13 +1,13 @@
 import './ApiTest.css';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import ReactPaginate from 'react-paginate';
 function ApiTest() {
 
     const [weather,setWeather] = useState([]);
+    const [data,setData] = useState([]);
     useEffect(() => {
-        document.getElementById('wth').style.borderBottom= "2px solid red";
-        document.getElementById('wth').style.color = 'red';
-        document.getElementById('wth').style.fontWeight = 'bold';
+        document.getElementById('wth').classList.add('api-nav-active');
         let fetchIp = async () => {
             await fetch('https://api.ipify.org/?format=json')
             .then(res=>res.json())
@@ -20,7 +20,6 @@ function ApiTest() {
                     }
                 }).then((res)=>res.json())
                 .then((r)=>{
-                    console.log(r);
                     setWeather(r);
                 })
                 .catch((error)=>console.log(error));
@@ -30,13 +29,88 @@ function ApiTest() {
         fetchIp();
     }, []);
 
+    let showNews = async () => {
+        document.getElementById('wth').classList.remove('api-nav-active');
+        document.getElementById('gam').classList.add('api-nav-active');
+        document.getElementById('weather').style.display = 'none';
+        document.getElementById('gameNews').style.display = 'flex';
+
+
+        await fetch("https://gaming-news.p.rapidapi.com/news", {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "gaming-news.p.rapidapi.com",
+                "x-rapidapi-key": "a5bb063c13msh224ca114d820861p125720jsn3e6cc3d310c1"
+            }
+        }).then((res)=>res.json())
+        .then((r)=>{
+            setData(r);
+            
+        }).catch((error)=>console.log(error));
+        
+    }
+
+    let showWeather = async () => {
+        document.getElementById('wth').classList.add('api-nav-active');
+        document.getElementById('gam').classList.remove('api-nav-active');
+        document.getElementById('weather').style.display = 'block';
+        document.getElementById('gameNews').style.display = 'none';
+
+        await fetch('https://api.ipify.org/?format=json')
+            .then(res=>res.json())
+            .then((r)=>{
+                fetch("https://weatherapi-com.p.rapidapi.com/current.json?q="+r.ip, {
+                    "method": "GET",
+                    "headers": {
+                        "x-rapidapi-host": "weatherapi-com.p.rapidapi.com",
+                        "x-rapidapi-key": "a5bb063c13msh224ca114d820861p125720jsn3e6cc3d310c1"
+                    }
+                }).then((res)=>res.json())
+                .then((r)=>{
+                    setWeather(r);
+                })
+                .catch((error)=>console.log(error));
+            })
+            .catch((error)=>console.log(error));
+    }
+
+    let [pageNumber, setPageNumber] = useState(0);
+    let perPage = 5;
+    let pagesVisited = pageNumber * perPage;
+       
+        
+        
+    
+    let  displayProd;
+    let pageCount;
+
+    if(data) {
+         displayProd =  data.slice(pagesVisited, pagesVisited+perPage).map((news)=>
+            { return(
+                <div className='news'>
+                                <div className='news-title'>{news.title}</div>
+                                <div className='news-src'>@{news.source}</div>
+                                <div className='news-url'><a href={news.url} target='_black'>{news.url}</a></div>
+                </div>
+                )
+            });
+        pageCount = Math.ceil(data.length/perPage);
+    } else {
+        pageCount = 0;
+        displayProd = "Nothing here";
+    }
+
+    const changePage = ({ selected }) => {
+        setPageNumber(selected);
+    }
+
     return(
         <>
         
             <nav id="back"><Link to='/'><span>Back</span></Link></nav>
             <div className="api-nav">
-                <nav id='wth'>Weather</nav>
-                <nav>Another</nav>
+                <nav id='wth' onClick={showWeather} >Weather</nav>
+                <nav id='gam' onClick={showNews}>Game News</nav>
                 <nav>another 1</nav>
             </div>
             <div id='weather'>
@@ -53,7 +127,33 @@ function ApiTest() {
                         <div>Gust: {weather.current.gust_mph+"mph"}</div>
                     </div>
                 </div> 
-                : "nothing"
+                : <div className='load-symb'><img src="/img/loadin.svg"  alt="error404" /></div>
+            }
+            </div>
+            <div id='gameNews'>
+            { data ?
+                <div className='api-body'>
+                    {displayProd}
+                <div style={{'display':'flex','justifyContent':'center','alignItems':'center','minHeight':'7em'}}>
+                {  pageCount > 1 ?
+                    <ReactPaginate
+                    previousLabel={"Previous"}
+                    nextLabel = {"Next"}
+                    pageCount = {pageCount}
+                    onPageChange = {changePage}
+                    containerClassName = {"pagi-div"}
+                    previousLinkClassName = {"page-link"}
+                    nextLinkClassName = {"page-link"}
+                    disabledClassName = {"page-item dissabled"}
+                    disabledLinkClassName = {"page-link"}
+                    activeClassName = {"page-item active"}
+                    pageClassName = {"page-item"}
+                    pageLinkClassName = {"page-link"}
+                    /> : null
+                }
+                </div>
+                </div> 
+                : "LOADING..."
             }
             </div>
         </>
